ebfc9741347de3c95204fb6756bc16fad1c26e17
Tongayi <chototongayi@gmail.com>
2018-03-07 19:03:26 -0500

    change file name
    change file name

--- print.py
+++ print.py
@@ -1,11 +1,11 @@
 # IMPORT
-# import contract smartContracts/01-helloWorld.avm "" 01 False False
+# import contract smartContracts/01-print.avm "" 01 False False
 #
 # INVOKE
 # testinvoke <your_contract_hash>
 #
 # BUILD TEST
-# build smartContracts/01-helloWorld.py test "" 01 False False
+# build smartContracts/print.py test "" 01 False False
 
 def Main(operation):
 
